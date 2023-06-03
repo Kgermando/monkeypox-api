90c6304ba894b7db7386019dd1e8e5a044d1095f
Germain kataku <katakugermain@gmail.com>
2023-06-03 10:18:36 +0100

    ccc

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.enableCors();
   // app.enableCors({
   //   origin: 'http://localhost:4200',
   //   credentials: true
