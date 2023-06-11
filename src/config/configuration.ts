c882f5a5d53091ab5e69b22cccaf7ea143a5110d
Germain <katakugermain@gmail.com>
2023-06-11 17:57:04 +0100

    fix db

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,12 +1,12 @@
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
     database: {
-      url: process.env.DATABASE_URL,
-      // host: process.env.POSTGRES_HOST,
-      // port: parseInt(process.env.POSTGRES_PORT, 10),
-      // user: process.env.POSTGRES_USER,
-      // password: process.env.POSTGRES_PASS,
-      // db: process.env.POSTGRES_DATABASE,
+      // url: process.env.DATABASE_URL,
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASS,
+      db: process.env.POSTGRES_DATABASE,
     },
   }
 );
