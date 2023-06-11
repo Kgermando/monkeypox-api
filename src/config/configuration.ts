171fbbd016bd1c8c15d90676ddb93dbdec0a945a
Germain <katakugermain@gmail.com>
2023-06-11 21:16:01 +0100

    ghgh

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,5 @@
 export default () => ({
-    port: parseInt(process.env.PORT, 10) || 3000,
+    port: parseInt(process.env.PORT, 10) || 8000,
     database: {
       // url: process.env.DATABASE_URL,
       host: process.env.POSTGRES_HOST,
