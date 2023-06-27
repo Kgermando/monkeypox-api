6ef124de54c802f21182406b05f76aec1a12c144
Kgermando <katakugermain@gmail.com>
2023-06-27 23:35:23 +0100

    DashboardService

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
-import { ServiceService } from './service/service.service';
 
 @Module({
   controllers: [DashboardController],
-  providers: [DashboardService, ServiceService]
+  providers: [DashboardService]
 })
 export class DashboardModule {}
