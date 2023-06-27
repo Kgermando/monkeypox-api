bb946dcfad4c1f837143fb6addf546bb8293ac3d
Kgermando <katakugermain@gmail.com>
2023-06-27 19:09:14 +0100

    dashbaorddd

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -17,22 +17,87 @@ export class DashboardController {
         return this.dashboardService.suspectCount();
     }
 
+    @Get('suspect-count-pourcent')
+    async suspectCountPourcent() {
+        return this.dashboardService.suspectCountPourcent();
+    }
+
     @Get('deces-count')
     async decesCount() {
         return this.dashboardService.decesCount();
-    }
+    } 
 
+    @Get('deces-count-pourcent')
+    async decesCountPourcent() {
+        return this.dashboardService.decesCountPourcent();
+    }
 
     @Get('tranche-age')
     async trancheAge() {
         return this.dashboardService.trancheAge();
+    } 
+
+ 
+    @Get('stats-record-fiche')
+    async statsRecordFiche() {
+        return this.dashboardService.statsRecordFiche();
+    }
+
+    @Get('stats-record-user')
+    async statsRecordUser() {
+        return this.dashboardService.statsRecordUser();
+    }
+
+    @Get('nbre-patient-sexe')
+    async nbrPatientSexe() {
+        return this.dashboardService.nbrPatientSexe();
+    }
+
+    @Get('a-ete-contact-patient')
+    async eteContactPatient() {
+        return this.dashboardService.eteContactPatient();
+    }
+
+    @Get('a-ete-hospitalise')
+    async eteHospitalise() {
+        return this.dashboardService.eteHospitalise();
+    }
+
+    @Get('type-contact')
+    async typeContact() {
+        return this.dashboardService.typeContact();
+    }
+
+    @Get('fievre')
+    async fievre() {
+        return this.dashboardService.fievre();
+    }
+
+    @Get('eruption-cutanee')
+    async eruptionCutanee() {
+        return this.dashboardService.eruptionCutanee();
     }
 
 
+    @Get('deces-province')
+    async decesProvince() {
+        return this.dashboardService.decesProvince();
+    }
 
-    @Get('province')
-    async all() {
-        return this.dashboardService.province();
+    @Get('deces-annee')
+    async decesAnnee() {
+        return this.dashboardService.decesAnnee();
+    }
+
+
+    @Get('statut-epidemiologique')
+    async statutEpidemiologique() {
+        return this.dashboardService.statutEpidemiologique();
+    }
+
+    @Get('evolution-cas')
+    async evolutionCas() {
+        return this.dashboardService.evolutionCas();
     }
 
 }
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -4,31 +4,120 @@ import { DataSource } from 'typeorm';
 
 @Injectable()
 export class DashboardService {
-    
+
     constructor(
         @InjectDataSource() private dataSource: DataSource,
     ) {}
-       
+
     async patientCount () {
        return this.dataSource.query("SELECT COUNT(*) FROM patient;");
     }
 
     async suspectCount () {
-       return this.dataSource.query("SELECT COUNT(*) FROM epidemie WHERE 'statut'='Cas suspect';");
+       return this.dataSource.query("SELECT COUNT(*) FROM epidemie WHERE statut='Cas suspect';");
+    }
+
+    async suspectCountPourcent () {
+        return this.dataSource.query(`
+        WITH resultat AS (SELECT COUNT(id) AS total FROM epidemie) 
+        SELECT statut, COUNT(statut)*100/total AS pourcentage FROM resultat, 
+        epidemie WHERE statut='Cas suspect' GROUP BY total, statut;
+    `);
     }
 
     async decesCount () {
-       return this.dataSource.query("SELECT COUNT(*) FROM epidemie WHERE 'statut'='Décès';");
+       return this.dataSource.query("SELECT COUNT(*) FROM epidemie WHERE statut='Décès';");
+    }
+
+    async decesCountPourcent () {
+        return this.dataSource.query(`
+        WITH resultat AS (SELECT COUNT(id) AS total FROM epidemie) 
+        SELECT statut, COUNT(statut)*100/total AS pourcentage FROM resultat, 
+        epidemie WHERE statut='Décès' GROUP BY total, statut;
+    `);
     }
 
     async trancheAge () {
         return this.dataSource.query("SELECT fourchette_age, COUNT(fourchette_age) FROM patient GROUP BY fourchette_age;");
     }
 
-    async province () {
-       return this.dataSource.query("SELECT * FROM user;");
+    async statsRecordFiche () {
+        return this.dataSource.query("SELECT COUNT(*) FROM epidemie;");
+    }
+    async statsRecordUser () {
+        return this.dataSource.query("SELECT COUNT(*) FROM user;");
     }
 
+    async nbrPatientSexe () {
+        return this.dataSource.query(`
+            WITH resultat AS (SELECT COUNT(id) AS total FROM patient) 
+            SELECT sexe, COUNT(sexe)*100/total AS pourcentage FROM resultat, 
+            patient GROUP BY total, sexe;
+        `);
+    }
+
+    async eteContactPatient () {
+        return this.dataSource.query(`
+            WITH resultat AS (SELECT COUNT(id) AS total FROM epidemie) 
+            SELECT a_ete_contact_patient, COUNT(a_ete_contact_patient)*100/total AS pourcentage FROM resultat, 
+            epidemie GROUP BY total, a_ete_contact_patient;
+        `);
+    }
+
+    async eteHospitalise () {
+        return this.dataSource.query(`
+            WITH resultat AS (SELECT COUNT(id) AS total FROM epidemie) 
+            SELECT a_ete_hospitalise, COUNT(a_ete_hospitalise)*100/total AS pourcentage FROM resultat, 
+            epidemie GROUP BY total, a_ete_hospitalise;
+        `);
+    }
+
+    async typeContact () {
+        return this.dataSource.query(`
+            WITH resultat AS (SELECT COUNT(id) AS total FROM epidemie) 
+            SELECT type_contact, COUNT(type_contact)*100/total AS pourcentage FROM resultat, 
+            epidemie GROUP BY total, type_contact; 
+        `);
+    }
+
+    async fievre () {
+        return this.dataSource.query(`
+            WITH resultat AS (SELECT COUNT(id) AS total FROM epidemie) 
+            SELECT fievre, COUNT(fievre)*100/total AS pourcentage FROM resultat, 
+            epidemie GROUP BY total, fievre;
+        `);
+    }
+
+    async eruptionCutanee () {
+        return this.dataSource.query(`
+            WITH resultat AS (SELECT COUNT(id) AS total FROM epidemie) 
+            SELECT eruption_cutanee, COUNT(eruption_cutanee)*100/total AS pourcentage FROM resultat, 
+            epidemie GROUP BY total, eruption_cutanee;
+        `);
+    } 
+ 
+
+    async decesProvince () {
+        return this.dataSource.query(`
+            SELECT province, COUNT(province) 
+            FROM epidemie WHERE statut='Décès'  GROUP BY province;
+        `);
+    }
+
+    async decesAnnee () {
+        return this.dataSource.query("SELECT * FROM epidemie;");
+     }
+
+    async statutEpidemiologique () {
+        return this.dataSource.query("SELECT * FROM epidemie;");
+    }
+
+    async evolutionCas () {
+        return this.dataSource.query("SELECT * FROM epidemie;");
+    }
+
+
+ 
  
 }
  
